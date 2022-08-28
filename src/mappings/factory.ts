5cd8c081c08fef00c197a911eca60ed94ef5eb5e
Dowland Aiello <dowlandaiello@gmail.com>
2022-08-28 17:15:05 -0400

    Finish factory mappings.

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,14 +1,23 @@
 import { FactoryCreated, IdeaCreated } from "../generated/Factory/Factory";
-import { Registry } from "../generated/schema";
+import { Registry, Idea } from "../generated/schema";
 
 /**
  * Called when the registry gets created
  */
 export const handleFactoryCreated = (event: FactoryCreated) => {
-  event;
+	const reg = new Registry(event.params.registry);
+	reg.ideas = [];
+	reg.save();
 };
 
 /**
  * Called when an instance of the idea contract is created by the registry.
  */
-export const handleIdeaCreated = (event: IdeaCreated) => {};
+export const handleIdeaCreated = (event: IdeaCreated) => {
+	// Already created by handleIdeaRecorded
+	const idea = Idea.load(event.params.idea);
+
+	const reg = Registry.load(event.params.registry);
+	reg.ideas.push(idea.id);
+	reg.save();
+};
